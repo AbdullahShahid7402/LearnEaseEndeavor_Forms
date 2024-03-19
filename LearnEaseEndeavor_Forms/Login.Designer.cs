d8f96fbfede9e2be38a630d28509f1090ed59fc2
AbdullahShahid7402 <abdullahshahid210@gmail.com>
2024-03-19 19:19:42 +0500

    Add .gitignore and .gitattributes.

---
+++ LearnEaseEndeavor_Forms/Login.Designer.cs
@@ -0,0 +1,128 @@
+﻿
+namespace LearnEaseEndeavor_Forms
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
+            this.EmailPrompt = new System.Windows.Forms.Label();
+            this.PasswordPrompt = new System.Windows.Forms.Label();
+            this.EmailTextBox = new System.Windows.Forms.TextBox();
+            this.PasswordTextBox = new System.Windows.Forms.TextBox();
+            this.Validate = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // EmailPrompt
+            // 
+            this.EmailPrompt.AutoSize = true;
+            this.EmailPrompt.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.EmailPrompt.Location = new System.Drawing.Point(184, 177);
+            this.EmailPrompt.Name = "EmailPrompt";
+            this.EmailPrompt.Size = new System.Drawing.Size(89, 31);
+            this.EmailPrompt.TabIndex = 0;
+            this.EmailPrompt.Text = "Email:";
+            this.EmailPrompt.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // PasswordPrompt
+            // 
+            this.PasswordPrompt.AutoSize = true;
+            this.PasswordPrompt.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PasswordPrompt.Location = new System.Drawing.Point(131, 258);
+            this.PasswordPrompt.Name = "PasswordPrompt";
+            this.PasswordPrompt.Size = new System.Drawing.Size(142, 31);
+            this.PasswordPrompt.TabIndex = 1;
+            this.PasswordPrompt.Text = "Password:";
+            // 
+            // EmailTextBox
+            // 
+            this.EmailTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.EmailTextBox.Location = new System.Drawing.Point(279, 170);
+            this.EmailTextBox.Name = "EmailTextBox";
+            this.EmailTextBox.Size = new System.Drawing.Size(370, 38);
+            this.EmailTextBox.TabIndex = 2;
+            // 
+            // PasswordTextBox
+            // 
+            this.PasswordTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PasswordTextBox.Location = new System.Drawing.Point(279, 251);
+            this.PasswordTextBox.Name = "PasswordTextBox";
+            this.PasswordTextBox.Size = new System.Drawing.Size(370, 38);
+            this.PasswordTextBox.TabIndex = 3;
+            // 
+            // Validate
+            // 
+            this.Validate.Location = new System.Drawing.Point(713, 415);
+            this.Validate.Name = "Validate";
+            this.Validate.Size = new System.Drawing.Size(75, 23);
+            this.Validate.TabIndex = 4;
+            this.Validate.Text = "Login";
+            this.Validate.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(360, 50);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click_1);
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.Validate);
+            this.Controls.Add(this.PasswordTextBox);
+            this.Controls.Add(this.EmailTextBox);
+            this.Controls.Add(this.PasswordPrompt);
+            this.Controls.Add(this.EmailPrompt);
+            this.Name = "Login";
+            this.Text = "L E E";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label EmailPrompt;
+        private System.Windows.Forms.Label PasswordPrompt;
+        private System.Windows.Forms.TextBox EmailTextBox;
+        private System.Windows.Forms.TextBox PasswordTextBox;
+        private System.Windows.Forms.Button Validate;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
